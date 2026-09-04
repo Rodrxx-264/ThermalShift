2fd91df01baa4ba5c531aa9eca4da34118f67751
Rodrigo Imeri <rimeri@Rodrigos-Mac-mini.local>
2026-09-03 19:14:17 -0600

    proyecto nuevo

---
+++ ThermalShift/ThermalShift/ThermalShiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThermalShiftApp.swift
+//  ThermalShift
+//
+//  Created by Rodrigo Imeri on 3/09/26.
+//
+
+import SwiftUI
+
+@main
+struct ThermalShiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
